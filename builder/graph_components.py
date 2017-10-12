7471cb354e4d814e2f2dff2fee6319accd5d7631
cbizon <bizon@renci.org>
2017-10-12 08:19:18 -0400

    Beginning to add Node and Edge objects

---
+++ builder/graph_components.py
@@ -0,0 +1,7 @@
+class Node():
+    """Used as the node object in KnowledgeGraph.
+    
+    Instances of this class can be passed to WorldGraph/greent as query subjects/objects."""
+    def __init__(self,identifier,node_type):
+        self.id = idstring
+        self.type = node_type
